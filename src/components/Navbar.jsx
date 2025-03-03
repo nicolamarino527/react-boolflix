81d47a3f1f5cbce6c5fd95a395a5785732b420b5
nicolamarino <nicolamarino527@gmail.com>
2025-03-03 14:31:55 +0100

    [feat] add homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,9 @@ import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
 
+    // scriviamo la costante per l array di film
+    const [movies, setMovies] = useState([]);
+
     // costante per aggiornare la query
     const [inputQuery, setInputQuery] = useState("")
 
@@ -35,7 +38,7 @@ export default function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
+                                <NavLink to="/" className="nav-link active text-white" aria-current="page" href="#">Home</NavLink>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link text-white" href="#">Link</a>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,80 @@
-
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import Flag from "react-world-flags";
 
 export default function HomePage() {
+
+    // memorizziamo i dati
+    const [popular, setPopular] = useState([])
+    // errore
+    const [error, setError] = useState(null);
+
+
+    // creiamo la costante dove contenere le flags in base alla lingua tramite react-world-flags
+    const flags = {
+        it: "IT",
+        en: "US",
+        es: "ES",
+        fr: "FR",
+        ja: "JA"
+    };
+
+    // chiamata
+    useEffect(() => {
+        const fetchPopular = async () => {
+            try {
+                const response = await axios.get("https://api.themoviedb.org/3/tv/popular", {
+                    params: {
+                        api_key: "e99307154c6dfb0b4750f6603256716d",
+                        language: "it-IT",
+                        page: 1,
+                    },
+                });
+
+                setPopular(response.data.results);;
+            } catch (err) {
+
+                setError("Errore nel recupero dei dati");
+
+            }
+        };
+
+        fetchPopular();
+    }, []);
+
     return (
         <>
             <div className="main">
-                HomePage
+                <h1 className="pb-4 text-white">Uscite popolari</h1>
+                {/* <h1 className="text-white">Risultati di ricerca per:</h1> */}
+                {/* scriviamo un messaggio in caso di errore */}
+                {error && <p className="text-danger">{error}</p>}
+
+                {/* nel caso in cui non troviamo risultati mostrimo un messaggio all utente  */}
+                {popular.length > 0 ? (
+                    <ul className="card-home-container">
+                        {popular.map((pop) => (
+                            <li key={pop.id}>
+                                <div className="card pb-3" style={{ width: '25rem', height: "35rem" }}>
+                                    <img src={`https://image.tmdb.org/t/p/w342${pop.poster_path}`} className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-titl pb-2">{pop.title}</h5>
+                                        <p className="card-text">Titolo originale: {pop.original_title}</p>
+
+                                        {/* utilizziamo react-world-flags per cambiare la la descrizione del linguaggio nell immagine della bandiera */}
+                                        <p className="card-text">
+                                            <Flag className="flags" code={flags[pop.original_language.toLowerCase()] || "?"} ></Flag>
+                                        </p>
+
+                                        <p className="card-text">Voto: {pop.vote_average}</p>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>Nesun risultato trovato...</p>
+                )}
             </div>
         </>
     )
